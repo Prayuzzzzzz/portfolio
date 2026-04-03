91de31094a7e129fa8d6a1c9ee4cd96e9f23cb3d
prayuzzzzzz <prayuzz06@gmail.com>
2026-04-03 12:21:18 +0545

    second commit fixed

--- dist/dev/static/chunks/app_page_tsx_0kcl1js._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0hr2ox1._.js","static/chunks/node_modules_0i86hpc._.js"],
-    source: "dynamic"
-});
